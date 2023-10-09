5ad933adca2a43b2888a0b70ae8a55d3283d1ab9
Guilherme <guilhermeeschersalvador@gmail.com>
2023-10-09 20:49:15 -0300

    Update: Improvements in validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "formato de e-mail inválido" }
-  validates :cpf, format: { with: /\A\d{3}\.\d{3}\.\d{3}-\d{2}\z/, message: "formato de CPF inválido" }
-  validates :phone, format: { with: /\A\(\d{2}\) \d{9}\z/, message: "formato de telefone inválido" }
-end
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "formato de e-mail inválido" }
+    validates :cpf, format: { with: /\A\d{3}(\.\d{3}){2}-\d{2}|\d{11}\z/, message: "formato de CPF inválido" }
+    validates :phone, format: { with: /\A\(\d{2}\) \d{9}|\d{2} \d{9}|\(\d{2}\)\d{9}|\d{2}\d{9}\z/, message: "formato de telefone inválido" }
+  end
+  
